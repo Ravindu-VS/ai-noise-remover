7969094a64433386e2cb4c2b09f336286e77cbec
Ravindu Vinusha <ravinduvinushasahan@gmail.com>
2025-04-13 14:31:41 +0530

    Fix: re-added frontend as regular folder, not submodule

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImageUploader from './components/ImageUploader';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Image Denoising App</h1>
+      <ImageUploader />
+    </div>
+  );
+}
+
+export default App;
